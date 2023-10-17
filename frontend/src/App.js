9d0fbf3fad44cef015ae84a28f0a72f5c3cc4b97
codesheff@outlook.com <codesheff@outlook.com>
2023-10-17 15:16:15 +0100

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -12,18 +17,21 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 // The second is the function to be used to update the state value
 
 const App = () => {
+  // setImages and setWord execute asynchronously! So, change isn't done immediately
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
   // e is an event.
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
+    
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([{ ...data, title: word }, ...images]); // ... spread operator - get all the individual elements of an array. We add the current search term ('word') as the title
+        // console.log(images); // because setImages is asynchronout, images probably won't contain your updates at this point.
       })
       .catch((err) => {
         console.log(err);
@@ -31,14 +39,31 @@ const App = () => {
     setWord("");
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id)); // this will filter images , so that it does not contain the id we specify
+  };
+
   // When running in development mode, we can access process.env
   //console.log(process.env.REACT_APP_UNSPLASH_KEY);
   // When optimised build for prd, the variables are included directly in your application
+
+  // User map helper method to loop through all images in array , and returns a new array ( an array of image cards)
+  // image past as prop to ImageCard is images[i]
 
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
